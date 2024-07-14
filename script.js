adfdb397b02a3db372480a062cf98cbf3fb2d28c
Shivaprasad1028 <burashiva444@gmail.com>
2024-07-15 01:13:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    const toggleBtn = document.querySelector(".toggle-btn");
+    const dropdown = document.querySelector(".drop-btn");
+    toggleBtn.addEventListener("click", () =>{
+        dropdown.classList.toggle("top-16")
+    })
+   
